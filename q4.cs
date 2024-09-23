ad2f4f04eb011e212c9b0fde495d96cc06183c6a
venugopal5501 <venugopal14082002@gmail.com>
2024-09-23 14:17:22 +0530

    case

--- q4.cs
+++ q4.cs
@@ -1,50 +1,57 @@
 using System;
-class Program{
+class Program
+{
     public static string choice;
-    
-    public static string venugopal(){
+
+    public static string venugopal()
+    {
         Console.WriteLine("Please choose an option: ");
         Console.WriteLine("A - Call DemoQuestion3 ");
         Console.WriteLine("B - Call DemoQuestion4 ");
         Console.WriteLine("C - Call DemoQuestion5 ");
         Console.WriteLine("X - Exit ");
         Console.WriteLine("Enter your choice: ");
-        choice=Console.ReadLine();
+        choice = Console.ReadLine();
         // choice.ToLowerCase();
         return choice;
     }
-    public static void DemoQuestion3(){
+    public static void DemoQuestion3()
+    {
         Console.WriteLine("DemoQuestion3 has been called.");
     }
-    public static void DemoQuestion4(){
+    public static void DemoQuestion4()
+    {
         Console.WriteLine("DemoQuestion4 has been called.");
     }
-    public static void DemoQuestion5(){
+    public static void DemoQuestion5()
+    {
         Console.WriteLine("DemoQuestion5 has been called.");
     }
 
-    public static void Main(string []args){
+    public static void Main(string[] args)
+    {
         string input;
-        while(true){
-            input=venugopal();
-            switch(input.ToUpper())
+        while (true)
+        {
+            input = venugopal();
+            switch (input.ToUpper())
             {
-            
+
                 case "A":
-                DemoQuestion3();
-                break;
+                    DemoQuestion3();
+                    break;
                 case "B":
-                DemoQuestion4();
-                break;
+                    DemoQuestion4();
+                    break;
                 case "C":
-                DemoQuestion5();
-                break;
+                    DemoQuestion5();
+                    break;
                 case "X":
-                Console.WriteLine("Ends");
-                return;
+                    Console.WriteLine("Ends");
+                    return;
                 default:
-                Console.WriteLine("invalid choice please try again");
-                break;
+                    Console.WriteLine("invalid choice please try again");
+                    break;
 
             }
         }
